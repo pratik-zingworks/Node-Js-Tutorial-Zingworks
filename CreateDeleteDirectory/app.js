b96cc2b96ec6a7896a438e5b9050c64c1e9704bd
pratik sonar <pratik.sonar@zingworks.in>
2021-10-14 16:34:31 +0530

    Create-Delete-Directories

---
+++ CreateDeleteDirectory/app.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+//Delete File
+// fs.unlink('text.txt', (err)=>{
+//     console.log(err)
+// });
+
+//Create Directory
+// fs.mkdirSync('nodeApp')
+//or
+// fs.mkdir('nodeApp', (err)=>{console.log(err)})
+
+//Delete Directory
+// fs.rmdirSync('nodeApp')
+//or
+// fs.rmdir('nodeApp', (err)=>{console.log(err)})
